fb6498fd7e7a8523c458a19bb57b003ea1f1fb3c
Медик <duduevmuhammed@gmail.com>
2023-11-04 22:41:43 +0300

    fix: initial power

--- lib/p2p_game.dart
+++ lib/p2p_game.dart
@@ -24,7 +24,7 @@ const double resolutionAspect = 2319 / 1307;
 
 class P2PGame extends FlameGame with TapCallbacks, HasCollisionDetection, HasDraggablesBridge {
   /// Собранная сила солнца
-  int sunPower = 1000;
+  int sunPower = 100;
 
   /// Собранная сила ветра
   int electricity = 100;
